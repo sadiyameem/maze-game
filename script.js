395cb50dca497bbad1df730a89c2bac02021c1a4
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-12-31 15:21:36 -0500

    added the function for the walls

--- script.js
+++ script.js
@@ -49,4 +49,91 @@ restartGame = () => {
         return arr;
     };
     const grid = Array(cellVertical).fill(null).map(() => Array(cellsHorizontal).fill(false));
+
+    //walls
+    const verticals = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal -1 ).fill(false));
+
+    const horizontals = Array(cellsVertical).fill(null).map(() => Array(cellHorizontal).fill(false));
+
+    const startRow = Math.floor(Math.random() * cellsVertical);
+    const startColumn = Math.floor(Math.random() * cellsHorizontal);
+
+    const stepThroughCell = (row, column) => {
+        if (grid[row][column]) {
+            return;
+        }
+        grid[row][column] = true;
+        const neighbors = shuffle([
+            [row - 1, column, 'up'],
+            [row, column + 1, 'right'],
+            [row + 1, column, 'down'],
+            [row, column - 1, 'left']
+        ]);
+        for (let neighbor of neighbors) {
+            const [nextRow, nextColumn, direction] = neighbor;
+            if (nextRow < 0 || nextRow >= cellsVertical || nextColumn < 0 || nextColumn >= cellsHorizontal) {
+                continue;
+            }
+            if (gris[nextRow][nextColumn]) {
+                continue;
+            }
+            if (direction === 'left') {
+                verticals[row][column -1] = true;
+            } else if (direction === 'right') {
+                verticals[row][column] = true;
+            } else if (direction === 'up') {
+                horizontals[row -1][column] = true;
+            } else if (direction === 'down') {
+                horizontals[row][column] = true;
+            }
+            stepThroughCell(nextRow, nextColumn);
+        }
+    };
+    stepThroughCell(startRow, startColumn);
+    horizontals.forEach((row, rowIndex) => {
+        row.forEach((open, columnIndex) => {
+            if (open) {
+                return;
+            }
+            const wall = Bodies.rectangle(
+                columnIndex * unitLenghtX + unitLenghtX/2,
+                rowIndex * unitLenghtX + unitLenghtY,
+                unitLenghtX,
+                5,
+                {
+                    friction: 0,
+                    label: 'wall',
+                    isStatic: true,
+                    render: {
+                        fillStyle: 'rgb(216,191,216)'
+                    }
+                }
+            );
+            World.add(world, wall);
+        });
+    });
+    verticals.forEach((row, rowIndex) => {
+        row.forEach((open, columnIndex) => {
+            if (open) {
+                return;
+            }
+            const wall = Bodies.rectangle(
+                columnIndex * unitLenghtX + unitLenghtX,
+                rowIndex * unitLenghtY + unitLenghtY/2,
+                5,
+                unitLenghtY,
+                {
+                    friction: 0,
+                    label: 'wall',
+                    isStatic: true,
+                    render: {
+                        fillStyle: 'rgb(216,191,216)'
+                    }
+                }
+            );
+            World.add(world, wall);
+        });
+    });
+
+    // Goal
 }
